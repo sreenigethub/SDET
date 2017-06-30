422a402f9f86129d604aa5c3eb7525f310b6f9b4
sreenigethub <spashanam@gmail.com>
2017-06-30 14:38:55 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ class HelloWorld{
 		public static void main(String [] args){
 //THIS IS FOR SECOND PUSH
 			System.out.println("Welcome to java");
-                        for(int i=0;i<10;i++;){
+                        for(int i=0;i<10;i++){
 			  System.out.println(i); } 
 		}
 }
